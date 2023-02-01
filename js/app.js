2a01b13d187e9878372971e3c99bb409f3feb6b3
Alina232323 <alina.polianska@vergesense.com>
2023-02-01 08:50:00 -0800

    added time and a flip effect

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
-const gameCards = document.getElementsByClassName(".card")
+let timer = document.querySelector("#time")//timer is "time" on the game screen
 
-let cardFlipped = false;
-let card1
-let card2
+let second = 0 //begin timer count at 0
+let minute = 0 
 
-function flipCard(){
-    this.classlist.add("flip")
+//creating a timer
+const startTime =()=>{
+    second+=1
+    if(second >= 60){
+        minute +=1
+        second = 0      
+    }
+    let secondsOutput = second < 10 ? `0${second}`:second //if seconds value is less than 10 -> show 0 before the interger
+    let minutesOutput = minute < 10 ? `0${minute}`:minute 
+    let outputTimer = `${minutesOutput}:${secondsOutput}` //"time" output as shown on the screen
+
+    timer.innerHTML = outputTimer
 }
+startTime()
+
+let beginCount = setInterval(startTime, 1000)
+
+// adding a flip effect
+let cards = document.querySelectorAll(".card") //select all cards
+
+function flippedCard(){ 
+    this.classList.toggle("flip") //add flip to all cards
+}
+cards.forEach(a => {
+    a.addEventListener("click", flippedCard) //trigger flip with a click
+})
+
 
 
