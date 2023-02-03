7c7d7ec0c877f08dcaaa9afcfd2961395a94fb25
Alina232323 <alina.polianska@vergesense.com>
2023-02-03 00:08:53 -0800

    adjusted content and styling

--- js/app.js
+++ js/app.js
@@ -1,69 +1,20 @@
-document.querySelector("section").classList.toggle("hide")
-let timer = document.querySelector("#time") //timer is "time" on the game screen
-let beginCount
+document.querySelector("section").classList.toggle("hide", true)
+document.querySelector(".end-game-page").classList.toggle("hide", true)
+
+const endGameButton = document.querySelector("#end-game-button")
+endGameButton.addEventListener("click", function() {
+    endGame()
+})
 
 const startButton = document.querySelector("#start-button")
-startButton.addEventListener("click", function(){
+startButton.addEventListener("click", function() {
     startGame()
     setNames()
     shuffle()
-    beginCount = setInterval(startTime, 1000)//fires up the timer
+    beginCount = setInterval(startTime, 1000) //fires up the timer
 })
-
-let inputName1 
-let inputName2
-let outputName1
-let outputName2
-let playerOneName
-let playerTwoName
-let playerTurn = document.querySelector("#current-player")
-let player1Score = document.querySelector("#score1")
-let player2Score = document.querySelector("#score2")
-let currentPlayerScore 
-
-//function to print names on the screen
-function setNames(){
-
-    inputName1 = document.querySelector("#player1").value
-    outputName1 = document.querySelector("#name1")
-    inputName2 = document.querySelector("#player2").value
-    outputName2 = document.querySelector("#name2")
-   
-   // if input is empty, set players name
-    if (inputName1 == "" || inputName2 == "") {
-       inputName1 = "Player1"
-       inputName2 = "Player2"
-    }
-   
-   outputName1.innerHTML = inputName1
-   outputName2.innerHTML = inputName2
-   playerOneName = inputName1
-   player1Score.innerHTML = 0
-   playerTwoName = inputName2
-   player2Score.innerHTML = 0
-   playerTurn.innerHTML = playerOneName
-
-   }
-//function to switch turns for players if cards haven't matched  
-function switchPlayer() {
-    if (playerTurn.innerHTML === playerOneName) {
-        playerTurn.innerHTML = playerTwoName
-        currentPlayerScore = player2Score.innerHTML
-        return
-    } else {
-        playerTurn.innerHTML = playerOneName
-        currentPlayerScore = player1Score.innerHTML
-    }
-}
-   
-
-
-
-function startGame(){
-    document.querySelector(".landing-page").classList.toggle("hide")
-    document.querySelector("section").classList.toggle("hide")
-    startTime()
-}
+let timer = document.querySelector("#time") //timer is "time" on the game screen
+let beginCount
 
 let second = 0 //begin timer count at 0
 let minute = 0
@@ -81,9 +32,63 @@ const startTime = () => {
     let outputTimer = `${minutesOutput}:${secondsOutput}` //"time" output as shown on the screen
 
     timer.innerHTML = outputTimer
-    if (minutesOutput >= 1){
+    if (minutesOutput >= 1) {
         clearInterval(beginCount)
+        endGame()
     }
+
+}
+
+let inputName1
+let inputName2
+let outputName1
+let outputName2
+let playerOneName
+let playerTwoName
+let playerTurn = document.querySelector("#current-player")
+let player1Score = document.querySelector("#score1")
+let player2Score = document.querySelector("#score2")
+let currentPlayerScore
+
+//function to print names on the screen
+function setNames() {
+
+    inputName1 = document.querySelector("#player1").value
+    outputName1 = document.querySelector("#name1")
+    inputName2 = document.querySelector("#player2").value
+    outputName2 = document.querySelector("#name2")
+
+    // if input is empty, set players name
+    if (inputName1 == "" || inputName2 == "") {
+        inputName1 = "Player1"
+        inputName2 = "Player2"
+    }
+
+    outputName1.innerHTML = inputName1
+    outputName2.innerHTML = inputName2
+    playerOneName = inputName1
+    player1Score.innerHTML = 0
+    playerTwoName = inputName2
+    player2Score.innerHTML = 0
+    playerTurn.innerHTML = playerOneName
+
+}
+//function to switch turns for players if cards haven't matched  
+function switchPlayer() {
+    if (playerTurn.innerHTML === playerOneName) {
+        playerTurn.innerHTML = playerTwoName
+        currentPlayerScore = player2Score.innerHTML
+        return
+    } else {
+        playerTurn.innerHTML = playerOneName
+        currentPlayerScore = player1Score.innerHTML
+    }
+}
+
+function startGame() {
+    document.querySelector(".landing-page").classList.toggle("hide", true)
+    document.querySelector("section").classList.toggle("hide", false)
+    startTime()
 }
 
 // adding a flip effect
@@ -94,10 +99,10 @@ let card2
 let cardIsFlipped = false //cards are not flipped on default, and are facing backwards
 let cardIsLocked = false
 
-function flippedCard() {
-    if(cardIsLocked) {
+function flipTheCard() {
+    if (cardIsLocked) {
         return
-    }else if (this === card1){ //if the same card has been clicked twice
+    } else if (this === card1) { //if the same card has been clicked twice
         return
     }
 
@@ -114,11 +119,11 @@ function flippedCard() {
 
 function isAMatch() {
     if (card1.dataset.id === card2.dataset.id) { //if datasets of these cards are the same - the cards have matched
-        // currentPlayerScore +1 
-        if (playerTurn.innerHTML === playerOneName){
-            player1Score.innerHTML ++
-        }else if(playerTurn.innerHTML === playerTwoName){
-            player2Score.innerHTML ++
+
+        if (playerTurn.innerHTML === playerOneName) {
+            player1Score.innerHTML++
+        } else if (playerTurn.innerHTML === playerTwoName) {
+            player2Score.innerHTML++
         }
         lockTheCards()
         return true
@@ -127,38 +132,64 @@ function isAMatch() {
     }
 }
 
-function lockTheCards() { //lock the cards that have matched, so they don't flip back
-    card1.removeEventListener("click", flippedCard)
-    card2.removeEventListener("click", flippedCard)
 
-    reverseTheBoard()
+function lockTheCards() { //lock the cards that have matched, so they don't flip back
+    card1.removeEventListener("click", flipTheCard)
+    card2.removeEventListener("click", flipTheCard)
+    clearCardValues()
 }
 //flip the cards back if they don't match 
 function flipBack() {
     setTimeout(() => {
         card1.classList.remove("flip")
         card2.classList.remove("flip")
-        reverseTheBoard()
+        clearCardValues()
         switchPlayer()
     }, 1000)
 }
 
 cards.forEach(el => { //a loop that runs through all cards from the ".card" classlist
-    el.addEventListener("click", flippedCard) //trigger flip with once clicked
+    el.addEventListener("click", flipTheCard) //trigger flip with once clicked
 })
 
-function reverseTheBoard(){ //reverse the cards to their starting position
+function clearCardValues() { //empty cards' values
     cardIsLocked = false
     cardIsFlipped = false
     card1 = null
     card2 = null
 }
 
-function shuffle(){ //shuffle the board
-    cards.forEach(el =>{
+function shuffle() { //shuffle the board
+    cards.forEach(el => {
         let shuffled = Math.floor(Math.random() * 10)
         el.style.order = shuffled
+        player1Score.innerHTML = 0
+        player2Score.innerHTML = 0
     })
+   
 }
 
+const restartButton = document.querySelector("#restart-button")
+restartButton.addEventListener("click", function() {
+    clearCardValues()
+    shuffle()
+})
 
+let endGameMessage = document.querySelector("#end-game-message")
+
+function endGame() {
+
+    document.querySelector("section").classList.toggle("hide", true)
+    document.querySelector(".landing-page").classList.toggle("hide", true)
+    document.querySelector(".end-game-page").classList.toggle("hide", false)
+
+    if (player1Score.innerHTML > player2Score.innerHTML) {
+        endGameMessage.innerHTML = `Congratulations, the challenge is over! ${playerOneName} is a winner!`
+    } else if (player1Score.innerHTML < player2Score.innerHTML) {
+        endGameMessage.innerHTML = `Congratulations, the challenge is over! ${playerTwoName} is a winner!`
+    } else if (player1Score.innerHTML = player2Score.innerHTML && player1Score.innerHTML + player2Score.innerHTML != 0) {
+        endGameMessage.innerHTML = `Congratulations, the challenge is over! It's a draw!`
+    } else
+        endGameMessage.innerHTML = `Oops, no winners at this time! Try again!`
+    return
+}
